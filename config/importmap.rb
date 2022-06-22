47339287e7580d26569deb6c6e60d6257550c456
joyfuI <1342862+joyfuI@users.noreply.github.com>
2022-06-22 22:49:17 +0900

    디자인 라이브러리로 materialize 적용

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin '@materializecss/materialize', to: 'https://ga.jspm.io/npm:@materializecss/materialize@1.1.0/dist/js/materialize.js', preload: true
